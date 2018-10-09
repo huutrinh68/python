c8e1d53aa68d65a2fce7a7508bf436de3866959c
huutrinh68 <trinhsp89@gmail.com>
2018-10-09 10:21:59 +0900

    Create convert_datetime_timestamp.py
    2 method to convert string to datetime and plus 2 hours

---
+++ convert_datetime_timestamp.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+from datetime import timedelta
+
+"""
+Plus 2 hours into specify time
+"""
+
+# method1
+def cal_date(strs):
+    date = datetime.datetime.strptime(strs, "%Y-%m-%d %H:%M:%S")
+    unixtime = time.mktime(date.timetuple())
+    # Plus 2 hours
+    unixtime += 2*3600
+    date = datetime.datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S')
+    return date
+
+# method2
+def cal_date(strs):
+    # ex. strs = "2018-10-01 00:00:00"
+    date = datetime.datetime.strptime(strs, '%Y-%m-%d %H:%M:%S')
+    return date+timedelta(hours=24)
+    
